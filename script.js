b16bc283ba308dc00054b7f9c8536e5d798a204e
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-11-13 21:15:14 +0600

    Code refactoring.

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ var ContactInfoManagement = {
 		var _this=ContactInfoManagement;
 		_this.tbClients= JSON.parse(localStorage.getItem('tbClients'));
 		_this.matchClients=JSON.parse(localStorage.getItem('matchClients'));
-		_this.renderContactTabel();
+		_this.renderContactTabel(_this.tbClients);
 	}
-	, renderContactTabel: function () {
-		var contactList=ContactInfoManagement.tbClients;
+	, renderContactTabel: function (contactList) {
 		$("#tblList tbody").html("");
 		for (var i in contactList) {
 			var cli = JSON.parse(contactList[i]);
@@ -18,7 +17,7 @@ var ContactInfoManagement = {
 				"	<td>" + cli.Mobile + "</td>" +
 				"	<td>" + cli.Email + "</td>" +
 				"	<td>" + cli.Address + "</td>" +
-				"	<td><img src='edit.png' alt='Edit' onclick='BtnEdit(" + i + ")' class='btnEdit'/><img src='delete.png' alt='Delete' onclick='BtnDelete(" + i + ")' class='btnDelete'/></td>" +
+				"	<td><img src='edit.png' alt='Edit' onclick='ContactInfoManagement.btnEdit(" + i + ")' class='btnEdit'/><img src='delete.png' alt='Delete' onclick='ContactInfoManagement.btnDelete(" + i + ")' class='btnDelete'/></td>" +
 				"</tr>");
 		}
 
@@ -94,53 +93,33 @@ var ContactInfoManagement = {
 		ContactInfoManagement.tbClients.push(client);
 		localStorage.setItem("tbClients", JSON.stringify(ContactInfoManagement.tbClients));
 		fname.value=lname.value=email.value=mobile.value=address.value='';
-		ContactInfoManagement.renderContactTabel();
+		ContactInfoManagement.renderContactTabel(ContactInfoManagement.tbClients);
 
 	}
 	, searchContact: function () {
-		var key = document.getElementById('searchbox').value;
-		if (key == "" || key == null) {
+		var key = searchbox.value;
+		if (!key) {
 			alert("no value is given")
 			return false;
 		}
-		matchClients = [];
+		ContactInfoManagement.matchClients = [];
+		var tbClients=ContactInfoManagement.tbClients;
 		for (var i in tbClients) {
 			var cli = JSON.parse(tbClients[i]);
 			if (cli.Mobile == key) {
-
-				//console.log(cli);
-				matchClients.push(tbClients[i]);
-				localStorage.setItem("matchClients", JSON.stringify(matchClients));
+				ContactInfoManagement.matchClients.push(tbClients[i]);
+				localStorage.setItem("matchClients", JSON.stringify(ContactInfoManagement.matchClients));
 			}
 		}
-		document.getElementById('searchbox').value = "";
-		RenderContactTabel(matchClients);
+		searchbox.value = '';
+		ContactInfoManagement.renderContactTabel(ContactInfoManagement.matchClients);
 	}
 	, formUpdate: function (index) {
-		if ($("#ufname").val() == "") {
-			alert("First Name is required; ")
-			return false;
+		if(!ufname.value || !ulname.value || !uemail.value || !umobile.value || !uaddress.value){
+			alert('Empty field is not allowed')
+			return;
 		}
-
-		if ($("#ulname").val() == "") {
-			alert("Last Name is required; ")
-			return false;
-		}
-
-		if ($("#umobile").val() == "") {
-			alert("Mobile is required; ")
-			return false;
-		}
-
-		if ($("#uemail").val() == "") {
-			alert(" Email is required; ")
-			return false;
-		}
-		if ($("#uaddress").val() == "") {
-			alert("Address is required; ")
-			return false;
-		}
-
+		var tbClients= ContactInfoManagement.tbClients;
 		tbClients[index] = JSON.stringify({
 			FirstName: $("#ufname").val(),
 			LastName: $("#ulname").val(),
@@ -149,14 +128,13 @@ var ContactInfoManagement = {
 			Address: $("#uaddress").val()
 		});//Alter the selected item on the table
 
-
 		localStorage.setItem("tbClients", JSON.stringify(tbClients));
-		document.getElementById('abc').style.display = "none";
-		RenderContactTabel(tbClients);
+		abc.style.display = "none";
+		ContactInfoManagement.renderContactTabel(tbClients);
 	}
 	, btnEdit: function (index) {
-		document.getElementById('abc').style.display = "block";
-		var cli = JSON.parse(tbClients[index]);
+		abc.style.display = "block";
+		var cli = JSON.parse(ContactInfoManagement.tbClients[index]);
 		$('#popupContact').html("");
 		$('#popupContact').append(
 			'<form id="updateform">' +
@@ -165,8 +143,8 @@ var ContactInfoManagement = {
 			'<label>Mobile:</label><input type="text" id="umobile" pattern="[0-9]{11}" value="' + cli.Mobile + '"><br>' +
 			'<label>Email:</label><input type="email" id="uemail" value="' + cli.Email + '"><br>' +
 			'<label>Address:</label><textarea name="address" id="uaddress" value="' + cli.Address + '"></textarea><br>' +
-			'<input type="button" value=" Update " onclick="FormUpdate(' + index + ')"/>' +
-			'<input type="button" value=" Cancel " onclick="CancelForm()"/>' +
+			'<input type="button" value=" Update " onclick="ContactInfoManagement.formUpdate(' + index + ')"/>' +
+			'<input type="button" value=" Cancel " onclick="ContactInfoManagement.cancelForm()" />' +
 			'</form>');
 
 		$("#uaddress").val(cli.Address);
@@ -174,189 +152,17 @@ var ContactInfoManagement = {
 	}
 	, btnDelete: function (index) {
 		var result = confirm("Are you absolutely sure you want to delete?");
+		tbClients= ContactInfoManagement.tbClients;
 		if (result) {
 			tbClients.splice(index, 1);
 			localStorage.setItem("tbClients", JSON.stringify(tbClients));
-			RenderContactTabel(tbClients);
+			ContactInfoManagement.renderContactTabel(tbClients);
 		}
+	}
+	, cancelForm: function(){
+		abc.style.display='none';
 	}
 };
-
-
-//var tbClients = localStorage.getItem("tbClients");//Retrieve the stored data
-//        tbClients = JSON.parse(tbClients);
-//
-//if(tbClients == null) //If there is no data, initialize an empty array
-//		tbClients = [];
-//
-//var matchClients = localStorage.getItem("matchClients");//Retrieve the stored data
-//    matchClients = JSON.parse(matchClients); //Converts string to object
-
-//var RenderContactTabel=function (contactList){
-//    $("#tblList tbody").html("");
-//  	for(var i in contactList){
-//			var cli = JSON.parse(contactList[i]);
-//		  	$("#tblList tbody").append("<tr>"+
-//										 "	<td>"+cli.FirstName+"</td>" +
-//										 "	<td>"+cli.LastName+"</td>" +
-//										 "	<td>"+cli.Mobile+"</td>" +
-//										 "	<td>"+cli.Email+"</td>" +
-//										 "	<td>"+cli.Address+"</td>" +
-//										 "	<td><img src='edit.png' alt='Edit' onclick='BtnEdit("+i+")' class='btnEdit'/><img src='delete.png' alt='Delete' onclick='BtnDelete("+i+")' class='btnDelete'/></td>" +
-//		  								 "</tr>");
-//		}
-//  }
-
-
-function ValidationCheck(formid) {
-		var val=$('#'+formid+' ul li:nth-child(1) input').val();
-		if(val=='' || val==null){
-			$('#'+formid+' ul li:nth-child(1) input').css('float','left');
-			$('#'+formid+' ul li:nth-child(1)').append('<p style="color:red;font:12px;">* FirstName is required.</p>');			
-			return false;
-		}
-		val=$('#'+formid+' ul li:nth-child(2) input').val();
-		if(val=='' || val==null){
-			$('#'+formid+' ul li:nth-child(2) p').text('* LastName is required.').css('color','red');			
-			return false;
-		}
-
-		val=$('#'+formid+' ul li:nth-child(3) input').val();
-		if(val=='' || val==null){
-			$('#'+formid+' ul li:nth-child(3) p').text('* Mobile is required.').css('color','red');			
-			return false;
-		}
-		val=$('#'+formid+' ul li:nth-child(4) input').val();
-		if(val=='' || val==null){
-			$('#'+formid+' ul li:nth-child(4) p').text('* Email is required.').css('color','red');			
-			return false;
-		}
-		val=$('#'+formid+' textarea').val();
-		if(val=='' || val==null){
-			$('#'+formid+' ul li:nth-child(5) p').text('* Address is required.').css('color','red');		
-			return false;
-		}
-		return true;
-	}
-
-function AddContact(){
-
-		if(ValidationCheck("addcontactform")!=true){
-			return ;	
-			
-		}
-
-		var client = JSON.stringify({
-			FirstName : $("#fname").val(),
-			LastName  : $("#lname").val(),
-			Mobile : $("#mobile").val(),
-			Email : $("#email").val(),
-			Address : $("#address").val()
-		});
-		tbClients.push(client);
-		localStorage.setItem("tbClients", JSON.stringify(tbClients));
-		$('#addcontactform input').val("");
-		$('#addcontactform p').text("");		
-		$('#addcontactform textarea').val("");
-		RenderContactTabel(tbClients);
-	}
-
-function Search(){
-	var key= document.getElementById('searchbox').value;
-		if(key=="" || key==null)
-		{
-			alert("no value is given")
-			return false;
-		}  	
-	matchClients = [];
-	for(var i in tbClients){
-			var cli = JSON.parse(tbClients[i]);
-			if(cli.Mobile==key)
-			{
-
-				//console.log(cli);
-				matchClients.push(tbClients[i]);
-				localStorage.setItem("matchClients", JSON.stringify(matchClients));
-			}
-		}
-	document.getElementById('searchbox').value="";
-	RenderContactTabel(matchClients);
-}
-
-function CancelForm(){
-	document.getElementById('abc').style.display = "none";
-}
-
-function FormUpdate(index){
-
-    if($("#ufname").val()==""){
-        alert("First Name is required; ")
-        return false;
-    }
-
-    if($("#ulname").val()==""){
-        alert("Last Name is required; ")
-        return false;
-    }
-
-    if($("#umobile").val()==""){
-        alert("Mobile is required; ")
-        return false;
-    }
-
-    if($("#uemail").val()=="")
-    {
-        alert(" Email is required; ")
-        return false;
-    }
-    if($("#uaddress").val()=="")
-    {
-        alert("Address is required; ")
-        return false;
-    } 
-
-		tbClients[index] = JSON.stringify({
-			FirstName : $("#ufname").val(),
-			LastName  : $("#ulname").val(),
-			Mobile : $("#umobile").val(),
-			Email : $("#uemail").val(),
-			Address : $("#uaddress").val()
-			});//Alter the selected item on the table
-
-
-		localStorage.setItem("tbClients", JSON.stringify(tbClients));		
-		document.getElementById('abc').style.display = "none";
-	    RenderContactTabel(tbClients);
-}
-
-function BtnEdit(index){
-  	document.getElementById('abc').style.display = "block";
-		var cli = JSON.parse(tbClients[index]);
-		$('#popupContact').html("");
-		$('#popupContact').append(
-			'<form id="updateform">'+
-            '<label>First Name:</label><input type="text" id="ufname" value="'+cli.FirstName+'"><br>'+
-            '<label>Last Name:</label><input type="text" id="ulname" value="'+cli.LastName+'"><br>'+
-            '<label>Mobile:</label><input type="text" id="umobile" pattern="[0-9]{11}" value="'+cli.Mobile+'"><br>'+
-            '<label>Email:</label><input type="email" id="uemail" value="'+cli.Email+'"><br>'+
-            '<label>Address:</label><textarea name="address" id="uaddress" value="'+cli.Address+'"></textarea><br>'+
-            '<input type="button" value=" Update " onclick="FormUpdate('+index+')"/>'+
-            '<input type="button" value=" Cancel " onclick="CancelForm()"/>'+
-            '</form>');
-
-		 $("#uaddress").val(cli.Address);		
-		 $("#ufname").focus();
-  }
-
-
-  function BtnDelete(index){
-  	var result = confirm("Are you absolutely sure you want to delete?");
-	if (result) {	
-		tbClients.splice(index, 1);
-		localStorage.setItem("tbClients", JSON.stringify(tbClients));
-		RenderContactTabel(tbClients);
-	}
-  }
 
 $(document).ready(function () {
 	ContactInfoManagement.init();
